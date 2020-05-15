1b11481ba8c58f4114a81c02ff756ca628ce63a2
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-15 12:45:27 +0000

    add-task-folder

---
+++ SequencesTask/SequenceGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace SequencesTask
+{
+    /// <summary>
+    /// Generator of the sequences.
+    /// </summary>
+    public static class SequenceGenerator
+    {
+        /// <summary>
+        /// Generates the Fibonacci sequence.
+        /// </summary>
+        /// <param name="count">Sequence length.</param>
+        /// <returns>The Fibonacci sequence of <paramref name="count"/> first numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 1.</exception>
+        public static IEnumerable<BigInteger> GetFibonacciNumbers(int count)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Generates the sequence of prime numbers.
+        /// </summary>
+        /// <param name="count">Sequence length.</param>
+        /// <returns>A sequence of <paramref name="count"/> first prime numbers.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 1.</exception>
+        public static IEnumerable<int> GetPrimeNumbers(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
